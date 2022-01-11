e7100ed07c35282600091640a741726342733c91
sehi <bsh8000a@naver.com>
2022-01-11 13:49:21 +0900

    배열재작업

---
+++ src/array/Start.java
@@ -0,0 +1,12 @@
+package array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("5개의 숫자를 입력하시오 1~5");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+    }
+}
