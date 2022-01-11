89ecaf4dd6959ce50bfcdb6c69333cddd249c438
sehi <bsh8000a@naver.com>
2022-01-11 13:51:13 +0900

    배열재작업

--- src/array/Start.java
+++ src/array/Start.java
@@ -1,12 +1,32 @@
 package array;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Start {
-    public static void main(String[] args) {
-        System.out.println("5개의 숫자를 입력하시오 1~5");
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        System.out.println(x);
+    public static void sort(int[] arr){
+        sort(arr, 0);
     }
+    public static void sort(int[] arr, int start){
+        if(start < arr.length -1){
+            int min_index = start;
+            for (int i = start; i < arr.length; i++){
+                if(arr[i] < arr[min_index]) min_index = i;
+            }
+            swap(arr, start, min_index);
+            sort(arr, start +1);
+        }
+    }
+    public static void swap (int[] arr, int index1, int index2){
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+    public static void array(int[] arr){
+        for (int data : arr){
+            System.out.print(data + ",");
+        }
+        System.out.println();
+    }
+   public static void main(String[] args) {
+       int[] arr = {6, 4, 10, 3, 7, 8, 5, 2, 9, 1};
+       sort(arr);
+       array(arr);
+   }
 }
