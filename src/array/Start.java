8099b23e5a7c4ed8606eb9ef3234fef42df55739
sehi <bsh8000a@naver.com>
2022-01-12 14:14:51 +0900

    static없애고 코드해석

--- src/array/Start.java
+++ src/array/Start.java
@@ -1,9 +1,9 @@
 package array;
 public class Start {
    public static void main(String[] args) {
-       Sort sort = new Sort();
-       int[] arr = {6, 4, 10, 3, 7, 8, 5, 2, 9, 1};
-       sort.sort(arr);
-       sort.array(arr);
+       Sort sort = new Sort(); //Sort.java 호출
+       int[] arr = {6, 4, 10, 3, 7, 8, 5, 2, 9, 1}; //배열 선언
+       sort.sort(arr); //오름차순 정렬 작업 
+       sort.array(arr); //출력
    }
 }
